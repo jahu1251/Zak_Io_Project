e28333cef8cf33f35a11e9711907d24133a3e3f0
ZychDev <SzymonZychDev@gmail.com>
2020-10-29 02:31:15 +0100

    value upgrade

--- search.cpp
+++ search.cpp
@@ -6,11 +6,32 @@
 #include<fstream>
 #include<string>
 
-std::map<std::string, std::vector<std::string>> wyszukiwanie(std::list<std::string> pliki) {
+std::map<std::string, std::vector<std::pair<std::string,int>>> wyszukiwanie(std::map<std::string, int> pliki) {
 
-    auto koniec = pliki.end();
-    std::map<std::string, std::vector<std::string>> mapa;
+    //auto koniec = pliki.end();
+    std::map<std::string, std::vector<std::pair<std::string,int>>>  mapa;
+    for (auto it = pliki.begin(); it != pliki.end(); ++it) 
+  {
 
+    std::ifstream plik(it->first);
+    while(!plik.eof())
+    {
+        std::string linijka;
+        getline(plik, linijka);
+        std::string szukany = "#include\"";
+        size_t miejsce = linijka.find(szukany);
+
+        if (miejsce != std::string::npos)
+            {
+              miejsce += szukany.size();
+              std::string nazwa_pliku = linijka.substr(miejsce);
+              mapa[it->first].push_back(std::make_pair(nazwa_pliku,it->second));                         
+            }
+
+    }
+     
+  }
+/*
     for (auto it = pliki.begin(); it != pliki.end(); ++it) {
 
         std::ifstream plik(*it);
@@ -31,6 +52,7 @@ std::map<std::string, std::vector<std::string>> wyszukiwanie(std::list<std::stri
                 }
             }
         }
-
+*/
     return mapa;
+    
     }
