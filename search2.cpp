a6c7e93f6176de66e7a7c6a9649b0d00310f3615
jahu1251 <56964174+jahu1251@users.noreply.github.com>
2020-11-02 12:19:28 +0100

    search2 using a compilator
    An attept of using compilator as a function lister

--- search2.cpp
+++ search2.cpp
@@ -4,43 +4,15 @@
 #include<vector>
 #include<fstream>
 #include<string>
-#include<regex>
-#include<sstream>
-#include<conio.h>
 
-std::map<std::string, std::vector<std::string>> wyszukiwanie2(std::list<std::string> pliki) {
-    auto koniec = pliki.end();
-    std::map<std::string, std::vector<std::string>> mapa2;
+class A {
+public:
+    A() __attribute__((deprecated)) {}
+};
+int main() {
+    A a;
+}
 
-    for (auto it = pliki.begin(); it != pliki.end(); ++it) {
+void wyszukiwanie2() {
 
-        std::cout << "Wywolanie funkcji dla " << *it << std::endl;
-        
-       // std::ifstream plik(*it);
-
-        std::regex wzorzec( "(?<!\\.|\\:)\\b\\w+[A-Za-z0-9_]+(?=\\()" );
-        std::ifstream inf;
-        inf.open( *it );
-    
-        int lineo = 0;
-        std::string line;
-        while( std::getline( inf, line ) )
-        {
-            ++lineo;
-            std::smatch wynik;
-            if( std::regex_search( line, wynik, wzorzec ) )
-            {
-                std::cout << "Wiersz " << lineo << " : " << line;
-                for( int i; i < wynik.size(); ++i )
-                    std::cout << "\tpasuje.[" << i << "]: " << wynik[ i ] << std::endl;
-            
-            }
-            else
-                std::cout << "nie pasuje.\n";
-        
-        }
-
-    }
-
-    return mapa2;
 }
